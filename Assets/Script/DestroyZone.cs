45acfa059671b6bbef12f6f5f63d3f0960a3dfde
CATK\A80347 <kimura_yoshiki@cyberagent.co.jp>
2016-10-21 13:22:05 +0900

    FIrst

---
+++ Assets/Script/DestroyZone.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/* DestroyZoneクラス
+	物体が落ちたらそのGameObjectを削除する処理
+*/
+public class DestroyZone : MonoBehaviour
+{
+	// メンバ変数
+	// スコアテキストのGameObject
+	public GameObject ScoreText;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	// 衝突判定処理
+	void OnCollisionEnter( Collision col )
+	{
+		// 衝突判定が起こったら衝突した側のGameObjectを削除、スコアを減算する
+		Destroy( col.gameObject );
+		Number score = ScoreText.GetComponent< Number >();
+		score.SubNumberCnt();
+
+	}
+
+
+}
---
+++ Assets/Script/GunToMainCamera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+/* GunToMainCameraクラス
+	クリックで弾を発射
+	スクリーン座標からワールド座標に変換して弾を飛ばします
+*/
+public class GunToMainCamera : MonoBehaviour
+{
+	// メンバ変数
+	// しいたけのプレハブ
+	public GameObject[] ShiitakeGunPrehab;
+	// スコアテキストのGameObject
+	public GameObject ScoreText;
+	// しいたけを飛ばす力
+	public float InitializationVelocity = 8.0f;
+	// しいたけを出現させる距離(奥行の位置)
+	public float SetDistance = 10.0f;
+	// しいたけの種類を変更するタイミング
+	public const int ChangePrehubCnt = 5;
+	// しいたけを撃った数をカウントし周期的に変える
+	private long _shotLoopCnt;
+
+	// Use this for initialization
+	void Start()
+	{
+		// 撃ったしいたけのループカウントを初期化
+		_shotLoopCnt = 0;
+
+		// しいたけをマウス左クリックで飛ばす処理(UniRx)
+		this.UpdateAsObservable().Where( _ => Input.GetMouseButtonDown( 0 ) )
+			.ThrottleFirst( TimeSpan.FromMilliseconds( 300 ) )
+			.Subscribe( x =>
+			OnMouseChick()
+			);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	// しいたけを飛ばす処理(マウス左クリック)
+	void OnMouseChick()
+	{
+		// 撃ったしいたけのカウントを監視
+		// 通常はカウント+1、しいたけが切り替わった時点で数値1に戻す
+		_shotLoopCnt = _shotLoopCnt == ChangePrehubCnt ? 1 : _shotLoopCnt + 1;
+		
+		// しいたけを切り替える処理
+		GameObject go = Instantiate( ShiitakeGunPrehab[ _shotLoopCnt / ChangePrehubCnt ], transform.position, transform.rotation ) as GameObject;
+		
+		// マウス座標取得、カメラから少し離れた位置から出すためZ値を変更
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = SetDistance;
+
+		// スクリーン座標からワールド座標への返還
+		Vector3 worldPoint = GetComponent< Camera >().ScreenToWorldPoint( mousePos );
+		Vector3 direction = ( worldPoint - transform.position ).normalized;
+		go.GetComponent< Rigidbody >().velocity = direction * InitializationVelocity;
+
+		// フィールド上にあるしいたけの数をカウント
+		Number score = ScoreText.GetComponent< Number >();
+		score.AddNumberCnt();
+
+	}
+
+}
