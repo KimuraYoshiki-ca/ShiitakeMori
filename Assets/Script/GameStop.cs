ee263d4bdd319b034da1a511fd2c1d2e9929b4db
CATK\A80347 <kimura_yoshiki@cyberagent.co.jp>
2016-10-28 16:38:49 +0900

    New Assets

---
+++ Assets/Script/GameStop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+public class GameStop : MonoBehaviour
+{
+	public Button MyselfButton;
+	public Number TimerObject;
+	public int ClickSpeed;
+
+	// Use this for initialization
+	void Start()
+	{
+		MyselfButton.OnClickAsObservable()
+			.Buffer(
+				( this.UpdateAsObservable().Where( _ => Input.GetMouseButtonDown( 0 ) ) )
+					.Throttle( TimeSpan.FromMilliseconds( ClickSpeed ) ) )
+			.Where( x => x.Count >= 2 )
+			.Subscribe( _ =>
+					TimerObject.SubNumberCnt( TimerObject.NumericValue )
+			);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+}
